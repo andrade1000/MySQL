acc0e597e8ed3e83ad9108dd4fe0987154ac3cb5
JHULY VELASQUEZ ANDRADE <jhuly.vandrade@senacsp.edu.br>
2022-05-16 16:55:45 -0300

    inicio do projeto

---
+++ agenda.sql
@@ -0,0 +1,17 @@
+/**
+Agenda de contatos
+@Jhuly Velasquez Andrade
+*/
+
+-- Verificar Banco de Dados
+show databases;
+
+-- Criar um novo Banco de Dados
+create database teste;
+create database agendajhuly;
+
+-- Excluir um banco de dados
+drop database teste;
+
+-- Selecionar o banco de dados
+use agendajhuly;
