9344be65547cb6c418765ec806d643e18d5f5c12
Jhuly velasquez andrade <jhulyvelasquez@gmail.com>
2022-05-23 17:17:10 -0300

    atualização da tabelas lojinha

--- lojinha2/script lojinha de doces.sql
+++ lojinha2/script lojinha de doces.sql
@@ -6,26 +6,33 @@ lojinha
 
 create database lojinhajhuly2;
 use lojinhajhuly2;
+
+-- timestamp default current_timestamp (data e hora automatica)
+-- date (tipo de datos relacionado a data YYYYMMDD)
 create table produtos(
     codigo int primary key auto_increment,
+    barcode varchar(255),
     produto varchar(255) not null,
-    quantidade int not null,
-    valor decimal(10,2)
+    descrição varchar(255) not null,
+    fabricante varchar(255) not null,
+    datacard timestamp default current_timestamp,
+    dataval date not null,
+    estoque int not null,
+    estoquemin int not null,
+    unidade varchar(255) not null,
+    localizacao varchar(255),
+    custo decimal(10,2) not null,
+    lucro decimal(10,2),
+    venda decimal(10,2)
     );
     describe produtos;
     
-insert into produtos (produto,quantidade,valor)
-values ('chocolate suflair','10','4.00');
-insert into produtos (produto,quantidade,valor)
-values ('Chiclete trident','23','2.00');
-insert into produtos (produto,quantidade,valor)
-values ('Pão de Mel Bauducco','9','8.00');
-insert into produtos (produto,quantidade,valor)
-values ('Alfajor Milka','18','3.50');
-insert into produtos (produto,quantidade,valor)
-values ('Bolinhos Ana Maria','26','4.60');
-insert into produtos (produto,quantidade,valor)
-values ('Refrigerante Coca-Cola','13','8.50');
+    insert into produtos(barcode,produto,escricao,fabricante,dataval,estoque,estoquemin,unidade,localizacao,custo,lucro,venda)
+    values
+    ('1234567899','chocolate','chocotale garoto','nestle',2013456,20,5,'un','salgadinho2',0.75,100,1.50);
+    
+    
+
 select * from produtos;
 
 select produto,quantidade,valor from produtos order by produto;
@@ -38,4 +45,35 @@ select * from produtos;
 
 insert into produtos (produto,quantidade,valor)
 values ('chiclete ','22','0.50');
+
+
+/**
+lojinha
+@author Jhuly Velasquez Andrade
+@version 1.1
+*/
+
+create table usuarios(
+    idusu int primary key auto_increment,
+    usuario varchar(255) not null,
+    login varchar(255) not null unique,
+    senha varchar(255) not null,
+    perfil varchar(255) not null
+    );
+
+    
+describe usuarios;
+
+-- para inserir uma senha com criptografia usamos md5()
+insert into usuarios(usuario,login,senha,perfil)
+values('administrador','admin',md5('admin'),'admin');
+insert into usuarios(usuario,login,senha,perfil)
+values('jhuly velasquez','jhuly_andrade',md5('123456'),'user');
+select * from usuarios;
+
+-- acessando o sistema pela tela de login
+-- and (função logica onde todas as condiçoes devem ser verdadeiras)
+select * from usuarios where login='admin' and senha=md5('admin');
+
+
     
